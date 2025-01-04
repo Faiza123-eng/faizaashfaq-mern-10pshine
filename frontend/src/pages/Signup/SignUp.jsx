2496de4753ce3436e9738b51764cf27efd4e86b1
Faiza123-eng <faizaashfaque7@gmail.com>
2025-01-05 03:26:42 +0600

    Add and update frontend files

--- frontend/src/pages/Signup/SignUp.jsx
+++ frontend/src/pages/Signup/SignUp.jsx
@@ -1,9 +1,37 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
+import "./Signup.css";
 
 const SignUp = () => {
   return (
-    <div>SignUp</div>
-  )
-}
+    <div className="signup-container">
+      <div className="signup-form">
+        <h2 className="form-title">Sign Up</h2>
+        <input
+          type="text"
+          placeholder="Full Name"
+          className="form-input"
+          required
+        />
+        <input
+          type="email"
+          placeholder="Email"
+          className="form-input"
+          required
+        />
+        <input
+          type="password"
+          placeholder="Password"
+          className="form-input"
+          required
+        />
+        <button className="btn-primary">Sign Up</button>
+        <div className="redirect-link">
+          <Link to="/login">Already have an account? Log In</Link>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default SignUp
+export default SignUp;
